4e1ec3fb2efe0395fee8844bf0124969e288dcea
Таня <polyarnbli@yandex.ru>
2019-05-28 12:46:39 +0300

    123

--- gulpfile.js
+++ gulpfile.js
@@ -75,10 +75,14 @@ include()
 
 gulp.task("server", function () {
   server.init({
-    server: "build/"
+    server: "build/",
+    notify: false,
+	    open: true,
+	    cors: true,
+	    ui: false
   });
 
-gulp.watch("source/less/**/*.less", gulp.series("css"));
+gulp.watch("source/sass/**/*.scss", gulp.series("css"));
 gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
 gulp.watch("source/*.html", gulp.series("html", "refresh")); });
 
